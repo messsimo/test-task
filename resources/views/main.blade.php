336dc09b8221ef483c8a25f5d838ba6d007543b7
Daniel Mihai <danikmihai23@mail.ru>
2024-11-16 06:36:53 +0200

    Feature: 1st block

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -8,6 +8,28 @@
     <title>Test Task</title>
 </head>
 <body>
+    <!-- Wrapper -->
+    <div class="wrapper">
+        <!-- Блок#1 -->
+        <div class="header-block">
+            <nav>
+                <a href="">About</a>
+                <a class="active" href="">Blog</a>
+                <a href="">Forum</a>
+                <a href="">Contact</a>
+            <nav>
+        </div>
 
+        <!-- Блок#2 -->
+        <!-- <div class="body-block">
+            <div class="describe">
+                <div class="container">
+                    <h2>01</h2>
+                    <img src="{{ asset('images/line.png') }}" alt="">
+                </div>
+                <h3>BLOG</h3>
+            </div>
+        </div> -->
+    </div>
 </body>
 </html>
